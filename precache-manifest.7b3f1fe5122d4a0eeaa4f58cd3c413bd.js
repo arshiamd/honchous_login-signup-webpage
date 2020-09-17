ffb13e6a172ad8dee5f71a2bbde50bcc6bdfd211
arshia <arshiamd.777@gmail.com>
2020-09-17 15:56:45 +0530

    Updates

---
+++ precache-manifest.7b3f1fe5122d4a0eeaa4f58cd3c413bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ea8a67dbfa22870d763c56a91b46c9",
+    "url": "/honchous_login-signup-webpage/index.html"
+  },
+  {
+    "revision": "0240b8f28f465b560fbb",
+    "url": "/honchous_login-signup-webpage/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "95fa5749653348465463",
+    "url": "/honchous_login-signup-webpage/static/css/main.1697ddbe.chunk.css"
+  },
+  {
+    "revision": "0240b8f28f465b560fbb",
+    "url": "/honchous_login-signup-webpage/static/js/2.078bc614.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/honchous_login-signup-webpage/static/js/2.078bc614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95fa5749653348465463",
+    "url": "/honchous_login-signup-webpage/static/js/main.d3baa412.chunk.js"
+  },
+  {
+    "revision": "fe0d9d70e597237d0026",
+    "url": "/honchous_login-signup-webpage/static/js/runtime-main.f69b51e1.js"
+  },
+  {
+    "revision": "5f63ec1d8eabb94e41550efbc2bdcc0b",
+    "url": "/honchous_login-signup-webpage/static/media/bg-image.5f63ec1d.jpeg"
+  }
+]);
